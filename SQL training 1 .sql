15b2d0eec32809d07a3c24daa23486a0c8a16a43
Marko <marko.huzanic96@gmail.com>
2021-09-10 11:16:02 +0200

    initial commit

---
+++ SQL training 1 .sql
@@ -0,0 +1,202 @@
+-- Creating Database and Tables 
+
+CREATE DATABASE book_shop;
+USE book_shop;
+
+--
+
+CREATE TABLE books 
+    (
+        book_id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(100),
+        author_fname VARCHAR(100),
+        author_lname VARCHAR(100),
+        released_year INT,
+        stock_quantity INT,
+        pages INT,
+        PRIMARY KEY(book_id)
+    );
+
+--
+
+INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+  VALUES ('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+  ('Norse Mythology', 'Neil', 'Gaiman', 2016, 43, 304),
+  ('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+  ('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+  ('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+  ('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+  ('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+  ('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+  ('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+  ('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+  ('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+  ("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+  ('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+  ('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+  ('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+  ('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+  
+-- CRUD
+
+SELECT * FROM books;
+
+--
+
+SELECT book_id FROM books;
+
+--
+
+SELECT CONCAT (author_fname, ' ',author_lname) AS full_name FROM books;
+
+--
+
+SELECT  DISTINCT CONCAT (author_fname, ' ',author_lname) AS full_name FROM books;
+
+--
+
+UPDATE author_fname SET pages = 292 WHERE title = 'The Namesake';
+
+--
+
+DELETE FROM books WHERE author_fname  = 'Neil';
+
+--
+
+SELECT  DISTINCT CONCAT (author_fname, ' ',author_lname) AS full_name FROM books;
+
+--String functions
+
+SELECT REPLACE(title, ' ', '->') AS title FROM books; 
+
+--
+
+SELECT author_lname AS forwards,REVERSE(author_lname) AS backwards 
+FROM books;
+
+--
+
+SELECT CONCAT(title, ' was released in ', released_year) AS blurb
+FROM books;
+
+--
+
+SELECT title,CHAR_LENGTH(title) AS 'character count'
+FROM books;
+
+--
+
+SELECT CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title',CONCAT(author_lname, ',', author_fname) AS author,CONCAT(stock_quantity, ' in stock') AS quantity
+FROM books;
+
+--Deeper selection
+
+SELECT title FROM books WHERE title LIKE '%stories%';
+
+--
+
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+
+--
+
+SELECT CONCAT(title, ' - ', released_year) AS summary 
+FROM books ORDER BY released_year DESC LIMIT 3;
+
+--
+
+SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+
+--
+
+SELECT title, released_year, stock_quantity 
+FROM books ORDER BY stock_quantity LIMIT 3;
+
+--
+
+SELECT title, author_lname 
+FROM books ORDER BY author_lname, title;
+
+--
+
+SELECT title, author_lname 
+FROM books ORDER BY 2,1;
+
+--
+
+SELECT CONCAT('MY FAVORITE AUTHOR IS ', UPPER(author_fname),' ', UPPER(author_lname),'!') AS yell
+FROM books ORDER BY author_lname;
+
+-- Aggregate Functions
+
+SELECT COUNT(*) FROM books;
+
+--
+
+SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
+
+--
+
+SELECT Sum(stock_quantity) FROM books;
+
+--
+
+SELECT author_fname, author_lname, AVG(released_year) FROM books GROUP BY author_lname, author_fname;
+
+--
+
+SELECT CONCAT(author_fname, ' ', author_lname) FROM books
+ORDER BY pages DESC LIMIT 1;
+
+--
+
+SELECT pages, CONCAT(author_fname, ' ', author_lname) FROM books
+ORDER BY pages DESC;
+
+--
+
+SELECT released_year AS year, COUNT(*) AS '# of books',AVG(pages) AS 'avg pages'
+FROM books GROUP BY released_year;
+
+-- Logical Operators
+
+SELECT title, released_year FROM books WHERE released_year > 1980;
+
+--
+
+SELECT title, author_lname FROM books WHERE author_lname IN ('Eggers','Chabon');
+
+--
+
+SELECT title, author_lname, released_year FROM books WHERE author_lname = 'Lahiri' AND released_year > 2000;
+
+--
+
+SELECT title, pages FROM books WHERE pages BETWEEN 100 AND 200;
+
+--
+
+SELECT title,author_lname 
+FROM books 
+WHERE author_lname LIKE 'C%' OR author_lname LIKE 'S%';
+
+--
+
+SELECT title,author_lname,
+  CASE
+    WHEN title LIKE '%stories%' THEN 'Short Stories'
+    WHEN title = 'Just Kids' OR
+      title = 'A Heartbreaking Work of Staggering Genius' THEN 'Memoir'
+    ELSE 'Novel'
+  END AS TYPE
+FROM books;
+
+--
+
+SELECT author_fname,author_lname,
+  CASE
+    WHEN COUNT(*) = 1 THEN '1 book'
+    ELSE CONCAT(COUNT(*), ' books')
+  END AS COUNT
+FROM books
+GROUP BY author_lname,author_fname;
+
---
+++ SQL training 2 .sql
@@ -0,0 +1,162 @@
+-- Creating Database and Tables 
+
+CREATE DATABASE university_store;
+USE university_store;
+
+--
+
+CREATE TABLE customers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+--
+
+CREATE TABLE orders(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+
+--
+
+INSERT INTO customers (first_name, last_name, email) 
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+
+--
+
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016/02/10', 99.99, 1),
+       ('2017/11/11', 35.50, 1),
+       ('2014/12/12', 800.67, 2),
+       ('2015/01/03', 12.50, 2),
+       ('1999/04/11', 450.25, 5);
+
+-- JOINS
+
+SELECT first_name, last_name, order_date, amount 
+FROM customers
+JOIN orders ON customers.id = orders.customer_id;
+
+--
+
+SELECT *
+FROM orders
+JOIN customers ON customers.id = orders.customer_id;
+
+--
+
+SELECT first_name, last_name, order_date, amount 
+FROM customers
+JOIN orders ON customers.id = orders.customer_id
+ORDER BY order_date;
+
+--
+
+SELECT first_name,last_name,UM(amount) AS total_spent
+FROM customers
+JOIN orders ON customers.id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY total_spent DESC;
+
+--
+
+SELECT * FROM customers
+LEFT JOIN orders ON customers.id = orders.customer_id;
+
+--
+
+SELECT first_name, last_name, order_date, amount
+FROM customers
+LEFT JOIN orders ON customers.id = orders.customer_id; 
+
+--
+
+SELECT first_name,last_name,IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders ON customers.id = orders.customer_id
+GROUP BY customers.id
+ORDER BY total_spent;
+
+--
+
+SELECT IFNULL(first_name,'MISSING') AS first,IFNULL(last_name,'USER') as last,order_date,amount,SUM(amount)
+FROM customers
+RIGHT JOIN orders ON customers.id = orders.customer_id
+GROUP BY first_name, last_name;
+
+--
+
+SELECT * FROM orders
+RIGHT JOIN customers ON customers.id = orders.customer_id;   
+
+
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100)
+);
+
+--
+
+CREATE TABLE papers (
+    title VARCHAR(100),
+    grade INT,
+    student_id INT,
+    FOREIGN KEY (student_id) 
+        REFERENCES students(id)
+        ON DELETE CASCADE
+);
+
+--
+
+INSERT INTO students (first_name)
+ VALUES ('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+
+--
+
+INSERT INTO papers (student_id, title, grade )
+ VALUES (1, 'My First Book Report', 60),
+(1, 'My Second Book Report', 75),
+(2, 'Russian Lit Through The Ages', 94),
+(2, 'De Montaigne and The Art of The Essay', 98),
+(4, 'Borges and Magical Realism', 89);
+
+--
+
+SELECT first_name, title, grade
+FROM students
+INNER JOIN papers ON students.id = papers.student_id
+ORDER BY grade DESC;
+
+--
+
+SELECT first_name,IFNULL(title, 'MISSING'),IFNULL(grade, 0)
+FROM students
+LEFT JOIN papers ON students.id = papers.student_id;
+
+--
+
+SELECT first_name,IFNULL(Avg(grade), 0) AS average, 
+    CASE 
+      WHEN Avg(grade) IS NULL THEN 'FAILING' 
+      WHEN Avg(grade) >= 75 THEN 'PASSING' 
+      ELSE 'FAILING' 
+    end AS passing_status 
+FROM students 
+LEFT JOIN papers ON students.id = papers.student_id 
+GROUP  BY students.id 
+ORDER  BY average DESC;
+
+
+
+
+
